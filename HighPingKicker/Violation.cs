55bd5f3eeb077fbac1a48f1ace8f3db54917b23c
Jonathan Robertson <jrob356@icloud.com>
2022-05-13 18:06:57 -0500

    remove unused imports

--- HighPingKicker/Violation.cs
+++ HighPingKicker/Violation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HighPingKicker {
+﻿namespace HighPingKicker {
     internal class Violation {
         public string SteamId { get; set; }
         public string Name { get; set; }
